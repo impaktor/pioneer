702027061bb6c29acff37d3e68dbe006b4baf436
Karl F <karlfogel@gmail.com>
2015-10-28 07:20:46 +0100

    Pirate fuel club: Gas Guzzler's Gathering added

--- data/modules/FuelClub/FuelClub.lua
+++ data/modules/FuelClub/FuelClub.lua
@@ -65,7 +65,7 @@ local flavours = {
 		nonmember_intro = l.FLAVOUR_0_NONMEMBER_INTRO,
 		member_intro = l.FLAVOUR_0_MEMBER_INTRO,
 		annual_fee = 400,
-		availability = {FED = 0.1, CIW = 0.1, HABER = 0, IND = 0.4} -- probability for these factions
+		availability = {FED = 0.1, CIW = 0.1, HABER = 0, IND = 0.4, PIRATE = 0} -- probability for these factions
 	},
 	{   -- SolFed club (reuse some messages)
 		clubname = l.FLAVOUR_1_CLUBNAME,
@@ -73,7 +73,7 @@ local flavours = {
 		nonmember_intro = l.FLAVOUR_1_NONMEMBER_INTRO,
 		member_intro = l.FLAVOUR_0_MEMBER_INTRO,
 		annual_fee = 400,
-		availability = {FED = 0.4, CIW = 0, HABER = 0, IND = 0}
+		availability = {FED = 0.4, CIW = 0, HABER = 0, IND = 0, PIRATE = 0}
 	},
 	{   -- Confederation club
 		clubname = l.FLAVOUR_2_CLUBNAME,
@@ -81,7 +81,7 @@ local flavours = {
 		nonmember_intro = l.FLAVOUR_2_NONMEMBER_INTRO,
 		member_intro = l.FLAVOUR_0_MEMBER_INTRO,
 		annual_fee = 300,
-		availability = {FED = 0, CIW = 0.4, HABER = 0, IND = 0}
+		availability = {FED = 0, CIW = 0.4, HABER = 0, IND = 0, PIRATE = 0}
 	},
 	{   -- Haber fuel division
 		clubname = l.FLAVOUR_3_CLUBNAME,
@@ -89,7 +89,15 @@ local flavours = {
 		nonmember_intro = l.FLAVOUR_3_NONMEMBER_INTRO,
 		member_intro = l.FLAVOUR_3_MEMBER_INTRO,
 		annual_fee = 600,
-		availability = {FED = 0, CIW = 0, HABER=1, IND = 0}
+		availability = {FED = 0, CIW = 0, HABER = 1, IND = 0, PIRATE = 0}
+	},
+	{   -- Pirate system
+		clubname = l.FLAVOUR_4_CLUBNAME,
+		welcome = l.FLAVOUR_0_WELCOME,
+		nonmember_intro = l.FLAVOUR_4_NONMEMBER_INTRO,
+		member_intro = l.FLAVOUR_4_MEMBER_INTRO,
+		annual_fee = 200,
+		availability = {FED = 0, CIW = 0, HABER = 0, IND = 0, PIRATE = 1}
 	}
 }
 
@@ -257,6 +265,9 @@ local onCreateBB = function (station)
 
 	-- For convenes, map long faction name to shorter table key
 	local faction_key
+
+	-- TODO if lawess: pirate faction!
+
 	if faction == "Solar Federation" then
 		faction_key = "FED"
 	elseif faction == "Commonwealth of Independent Worlds" then
