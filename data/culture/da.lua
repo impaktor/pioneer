bf869dc80145694ad730d621c5cec04c81dcc6e9
Karl F <karlfogel@gmail.com>
2021-05-30 10:56:06 +0200

    Add Danish names (only top 20 though)

---
+++ data/culture/da.lua
@@ -0,0 +1,73 @@
+-- Copyright © 2008-2021 Pioneer Developers. See AUTHORS.txt for details
+-- Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+-- https://www.dst.dk/da/Statistik/emner/befolkning-og-valg/navne/navne-i-hele-befolkningen
+
+local male = {
+	"Peter",
+	"Michael",
+	"Jens",
+	"Lars",
+	"Thomas",
+	"Henrik",
+	"Søren",
+	"Christian",
+	"Jan",
+	"Martin",
+	"Niels",
+	"Anders",
+	"Morten",
+	"Jesper",
+	"Mads",
+	"Rasmus",
+	"Hans",
+	"Per",
+	"Jørgen",
+	"Ole"
+}
+
+local female = {
+	"Anne",
+	"Kirsten",
+	"Mette",
+	"Hanne",
+	"Helle",
+	"Anna",
+	"Susanne",
+	"Lene",
+	"Maria",
+	"Marianne",
+	"Lone",
+	"Camilla",
+	"Pia",
+	"Louise",
+	"Charlotte",
+	"Tina",
+	"Gitte",
+	"Bente",
+	"Jette",
+	"Karen"
+}
+
+local surnames = {
+	"Nielsen",
+	"Jensen",
+	"Hansen",
+	"Andersen",
+	"Pedersen",
+	"Christensen",
+	"Larsen",
+	"Sørensen",
+	"Rasmussen",
+	"Jørgensen",
+	"Petersen",
+	"Madsen",
+	"Kristensen",
+	"Olsen",
+	"Thomsen",
+	"Christiansen",
+	"Poulsen",
+	"Johansen",
+	"Møller",
+	"Mortensen",
+}
