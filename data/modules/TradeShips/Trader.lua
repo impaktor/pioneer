54a9d93ece7b79ca33105ecee773213ef485a94e
Karl F <karlfogel@gmail.com>
2025-07-16 11:47:42 +0200

    Clean up dead code

--- data/modules/TradeShips/Trader.lua
+++ data/modules/TradeShips/Trader.lua
@@ -1,7 +1,6 @@
 -- Copyright © 2008-2026 Pioneer Developers. See AUTHORS.txt for details
 -- Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
 
-local Commodities = require 'Commodities'
 local Engine = require 'Engine'
 local Game = require 'Game'
 local HullConfig  = require 'HullConfig'
