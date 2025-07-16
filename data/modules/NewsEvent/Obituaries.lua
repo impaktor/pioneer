c0dffefaef9fa76ecc7f7689486b63c956dbc696
Karl F <karlfogel@gmail.com>
2025-07-16 15:39:15 +0200

    News article "Obituray"
    This aims mainly to share some lessons from other pilots' death,
    in the form of newspaper obituary.
    
    Unlike the commodity news, it does not change anything in the game for the
    player to take action on.

--- data/modules/NewsEvent/Obituaries.lua
+++ data/modules/NewsEvent/Obituaries.lua
@@ -32,6 +32,10 @@ local news = {}
 -- track if player has viewed this news before
 local interacted = {}
 
+local eventProbability = 0.2 -- probability of news generated when entering system
+local maxNumberNews = 1      -- max one (obituary) news at any given time
+local minTime = 2592000      -- no news the first 1 month of a new game (sec)
+
 -- add Traveller strings to flavours-table:
 for i = 1,news_indicies do
 	table.insert(flavours, {
@@ -71,9 +75,14 @@ local onChat = function (form, ref, option)
 	local ad = ads[ref]
 
 	form:Clear()
-	form:SetTitle(flavours[ad.n].headline)
 
 	local faction = Game.system.faction.name
+	local title = NewsEvent:get_title(faction)
+
+	-- local date = Game.time - Engine.rand:Number(0.1,0.9)*(*24*3600)
+	-- Format.DateOnly(date)
+
+	form:SetTitle(title)
 
 	if option == 0 then
 		form:SetMessage(flavours[ad.n].bodytext)
@@ -85,13 +94,13 @@ local onDelete = function (ref)
 	ads[ref] = nil
 end
 
--- when we enter a system the BBS is created and this function is called
 local onCreateBB = function (station)
 	-- create ads, (if any news)
-	for i, n in pairs(news) do
+
+	for i, _ in pairs(news) do
 		local idx = news[i].flavour_id
 		local ref = station:AddAdvert({
-			title       = flavours[idx].title,
+			title       = flavours[idx].headline,
 			description = flavours[idx].description,
 			icon        = "news",
 			onChat      = onChat,
@@ -108,14 +117,14 @@ local onEnterSystem = function (player)
 	if (not player:IsPlayer()) then return end
 
 	-- remove old news before making new
-	NewsEvent.checkOldNews(news)
+	NewsEvent:checkOldNews(news)
 
 	-- publication date, between 10 and 1 days ago
-	local date = NewsEvent.event_date()
+	local date = Game.time - (60*60*24) * Engine.rand:Number(0.1,0.9)
 
 	-- create a news event with low probability
-	if Engine.rand:Number(0,1) < NewsEvent.eventProbability and
-		#news < NewsEvent.maxNumberNews and Game.GetStartTime() > NewsEvent.minTime then
+	if Engine.rand:Number(0,1) < eventProbability and
+		#news < maxNumberNews and Game.GetStartTime() > minTime then
 		createNewsEvent(date)
 	end
 end
@@ -128,7 +137,7 @@ local onGameStart = function ()
 
 	if not loaded_data or not loaded_data.ads then return end
 
-	for k,ad in pairs(loaded_data.ads or {}) do
+	for _, ad in pairs(loaded_data.ads or {}) do
 		local ref = ad.station:AddAdvert({
 			title       = flavours[ad.n].headline,
 			description = flavours[ad.n].description,
@@ -155,6 +164,7 @@ Event.Register("onEnterSystem", onEnterSystem)
 Serializer:Register("Obituary", serialize, unserialize)
 
 
+local idx = 1
 debugView.registerTab("Obituary", {
 	icon = ui.theme.icons.news,
 	label = "News Obituary",
@@ -162,18 +172,28 @@ debugView.registerTab("Obituary", {
 	draw = function ()
 		ui.text("BBS Obituary debug")
 
-		ui.text("falvour: ".. #flavours)
-		ui.text("ads: " .. #ads)                   -- 0 ?
-		ui.text("news: " .. #news)                 -- 0 ?
-		ui.text("interacted: ".. #interacted)
+		ui.text("#falvour: ".. #flavours)
+		ui.text("#ads: " .. #ads)                   -- 0 ?
+		ui.text("#news: " .. #news)                 -- 0 ?
+		ui.text("#interacted: ".. #interacted)
 		ui.text("news_indicies: ".. news_indicies)
 
-		if ui.button("Render", Vector2(100, 0)) then
-			createNewsEvent(0)
+		if ui.button("Make news " .. idx, Vector2(100, 0)) then
+			local newsevent = {
+				flavour_id = idx,
+				expires = 0, --- xxx
+				publication_date = 0 ---xxx
+			}
+			table.insert(news, newsevent)
+			idx = idx + 1
 		end
 
-		for i, n in pairs(interacted) do
-			ui.text("Flavour:\t" ..  i .. "\t" .. tostring(interacted[i]))
+		if idx == news_indicies then
+			idx = 1
+		end
+
+		for i, _ in pairs(interacted) do
+			ui.text("Interacted with flavour:\t" ..  i .. "\t" .. tostring(interacted[i]))
 		end
 	end
 })
